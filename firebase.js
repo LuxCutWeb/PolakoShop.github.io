ece47e33872794c508ccc07e939fbed986c9a1b2
LuxCutWeb <luxcutweb@gmail.com>
2026-07-15 21:19:01 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -56,7 +56,7 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 // ID del administrador maestro (se almacena como campo en Firestore o UID definido)
-const ADMIN_EMAIL = "admin@polakoshop.com";
+const ADMIN_EMAIL = "jeremyalexandercubasomonte@gmail.com";
 /* ==========================================================================
    MÓDULO: AUTENTICACIÓN
    ========================================================================== */
